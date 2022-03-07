dfa49f57ab7c5a317e129ecb689a97fa29c2c664
Konstantine <Konstaphy@gmail.com>
2022-03-07 14:51:02 +0500

    hotfix: api models and db connection

--- client_ts/src/service/api/apiService.ts
+++ client_ts/src/service/api/apiService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { Endpoints } from "./endpoints";
-import UserDTO, { User } from "../../../../dtos/userDTO";
+import UserDTO, { UserFromDB } from "../../../../dtos/userDTO";
 
 const axiosInstance = axios.create({
     baseURL: "http://localhost:5000/api/",
@@ -15,6 +15,12 @@ axiosInstance.interceptors.request.use(config => {
 export class ApiService {
     public static GetCurrent = async (): Promise<UserDTO> => {
         const token = await axiosInstance.get(Endpoints.Refresh);
+        console.log(token);
+        localStorage.setItem("refreshToken", token.data.refresh_token);
+        return token.data.user;
+    };
+    public static Login = async (name: string, password: string): Promise<UserDTO> => {
+        const token = await axiosInstance.post(Endpoints.Login, { name, password });
         console.log(token);
         localStorage.setItem("refreshToken", token.data.refresh_token);
         return token.data.user;
--- client_ts/src/service/api/endpoints.ts
+++ client_ts/src/service/api/endpoints.ts
@@ -1,3 +1,4 @@
 export enum Endpoints {
     Refresh = "refresh",
+    Login = "login",
 }
