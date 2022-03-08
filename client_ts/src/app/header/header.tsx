097321728d332ae55888d485f99b38919e02ac36
Konstantine <Konstaphy@gmail.com>
2022-03-08 12:08:42 +0500

    hotfix: file system with links router

--- client_ts/src/app/header/header.tsx
+++ client_ts/src/app/header/header.tsx
@@ -1,24 +1,15 @@
 import React, { FC } from "react";
 import logo from "assets/logo.svg";
 import "./header.scss";
-import LoggedInHeaderLinks from "./loggedInHeaderLinks/loggedInHeaderLinks";
-import LoggedOutHeaderLinks from "./loggedOutHeaderLinks/loggedOutHeaderLinks";
-import { useTypedSelector } from "../../redux/store";
+import Links from "./links/links";
 
 const Header: FC = () => {
-    const state = useTypedSelector(state => state.user);
-
-    const renderLinks = () => {
-        if (state.user) {
-            return <LoggedInHeaderLinks />;
-        }
-        return <LoggedOutHeaderLinks />;
-    };
-
     return (
-        <main className={"header"}>
-            <img className={"header__logo"} src={logo} alt="whatyougonnado?" />
-            <div className={"header__links"}>{renderLinks()}</div>
+        <main className="header">
+            <img className="header__logo" src={logo} alt="whatyougonnado?" />
+            <div className="header__links">
+                <Links />
+            </div>
         </main>
     );
 };
